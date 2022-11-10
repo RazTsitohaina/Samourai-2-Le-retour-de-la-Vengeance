bd2c1eacbeb91d786d9ccca7e4d0d3ad8360e0cb
Crack <Crack@172.17.133.119>
2022-11-10 15:26:22 +0100

    Debut Ronin

--- src/histoires/MonHistoire.java
+++ src/histoires/MonHistoire.java
@@ -1,6 +1,11 @@
 package histoires;
+import personnages.Humain;
 
 
 public class MonHistoire {
-	
+	public static void main(String[] args){
+		Humain Test = new Humain("Tsito","Coca", 90);
+		Test.direbonjour();
+		Test.boire();
+	}
 }
---
+++ src/personnages/Ronin.java
@@ -0,0 +1,15 @@
+package personnages;
+
+public class Ronin extends Humain{
+	
+	/**
+	 * Constructeur du Ronin
+	 * @param nom
+	 * @param boisson_fav
+	 * @param argent
+	 */
+	public Ronin(String nom, String boisson_fav, int argent) {
+		super(nom,boisson_fav,argent);
+	}
+
+}
